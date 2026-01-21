3db0652f9be2cd055785307e52382fd3b0348d96
pratzzz2432 <pratzzz2432@gmail.com>
2026-01-21 13:37:46 +0530

    Exercise 3: GraphQL mutation to add reviews and return updated Movie

--- backend/homework/10/src/main/java/com/cinestream/controller/MovieController.java
+++ backend/homework/10/src/main/java/com/cinestream/controller/MovieController.java
@@ -1,11 +1,17 @@
 package com.cinestream.controller;
 
+import com.cinestream.dto.ReviewDto;
 import com.cinestream.entity.DirectorEntity;
 import com.cinestream.entity.MovieEntity;
 import com.cinestream.service.DirectorService;
 import com.cinestream.service.MovieService;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 @Controller
 public class MovieController {
@@ -21,8 +27,31 @@ public class MovieController {
         this.directorService = directorService;
     }
 
+    // -------- Exercise 1 & 2 --------
+
+    @QueryMapping
+    public MovieEntity findMovieById(@Argument Long id) {
+        return movieService.findMovieById(id);
+    }
+
     @SchemaMapping(typeName = "Movie", field = "director")
     public DirectorEntity director(MovieEntity movie) {
         return directorService.findById(movie.getDirectorId());
+    }
+
+    // -------- Exercise 3 --------
+
+    @MutationMapping
+    public MovieEntity addReview(
+            @Argument Long movieId,
+            @Argument String comment,
+            @Argument int rating
+    ) {
+        return movieService.addReview(movieId, comment, rating);
+    }
+
+    @SchemaMapping(typeName = "Movie", field = "reviews")
+    public List<ReviewDto> reviews(MovieEntity movie) {
+        return movieService.getReviews(movie.getId());
     }
 }
--- backend/homework/10/src/main/java/com/cinestream/service/MovieService.java
+++ backend/homework/10/src/main/java/com/cinestream/service/MovieService.java
@@ -1,23 +1,51 @@
 package com.cinestream.service;
 
-import com.cinestream.dto.MovieDto;
-import com.cinestream.repository.MovieRepository;
+import com.cinestream.dto.ReviewDto;
+import com.cinestream.entity.MovieEntity;
+import com.cinestream.repository.MovieJpaRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class MovieService {
 
     private static final Logger log = LoggerFactory.getLogger(MovieService.class);
-    private final MovieRepository repository;
 
-    public MovieService(MovieRepository repository) {
-        this.repository = repository;
+    private final MovieJpaRepository movieRepository;
+
+    // In-memory store for reviews (Exercise 3)
+    private final Map<Long, List<ReviewDto>> reviewsByMovie = new HashMap<>();
+
+    public MovieService(MovieJpaRepository movieRepository) {
+        this.movieRepository = movieRepository;
     }
 
-    public MovieDto findMovieById(String id) {
+    // -------- Exercise 1 & 2 --------
+
+    public MovieEntity findMovieById(Long id) {
         log.info("Fetching movie with id={}", id);
-        return repository.findById(id).orElse(null);
+        return movieRepository.findById(id).orElse(null);
+    }
+
+    // -------- Exercise 3 --------
+
+    public MovieEntity addReview(Long movieId, String comment, int rating) {
+        log.info("Adding review for movieId={}, rating={}", movieId, rating);
+
+        reviewsByMovie
+                .computeIfAbsent(movieId, k -> new ArrayList<>())
+                .add(new ReviewDto(comment, rating));
+
+        return findMovieById(movieId);
+    }
+
+    public List<ReviewDto> getReviews(Long movieId) {
+        return reviewsByMovie.getOrDefault(movieId, List.of());
     }
 }
